0e13128274dc21a792c0008b5b93c01aab2c9798
Matthew Wagerfield <matthew@wagerfield.com>
2014-07-14 22:16:18 +0100

    :gb: Implemented more Particle constructor tests

--- test/ParticleSpec.js
+++ test/ParticleSpec.js
@@ -2,6 +2,8 @@ describe('NDP.Particle(mass, opt_radius, opt_fixed, opt_dimensions)', function()
 
   beforeEach(function() {
     jasmine.addMatchers(CustomMatchers);
+    this.particleA = new NDP.Particle(1);
+    this.particleB = new NDP.Particle(2);
   });
 
   it('should be a Function Object', function() {
@@ -16,18 +18,36 @@ describe('NDP.Particle(mass, opt_radius, opt_fixed, opt_dimensions)', function()
     }).toThrow('Particle: mass must be a Number [kittens]');
   });
   it('should set [mass] to the first argument', function() {
+    expect(this.particleA.mass).toBe(1);
+    expect(this.particleB.mass).toBe(2);
   });
   it('should default [radius] to the value of [mass]', function() {
+    expect(this.particleA.radius).toEqual(this.particleA.mass);
+    expect(this.particleB.radius).toEqual(this.particleB.mass);
   });
   it('should set [radius] to the second argument if passed', function() {
+    this.particleA = new NDP.Particle(1, 2);
+    this.particleB = new NDP.Particle(3, 4);
+    expect(this.particleA.mass).toEqual(1);
+    expect(this.particleA.radius).toEqual(2);
+    expect(this.particleB.mass).toEqual(3);
+    expect(this.particleB.radius).toEqual(4);
   });
   it('should default [fixed] to false', function() {
+    expect(this.particleA.fixed).toBe(false);
   });
   it('should set [fixed] to the third argument if passed', function() {
+    this.particleA = new NDP.Particle(1, 2, true);
+    this.particleB = new NDP.Particle(3, 4, false);
+    expect(this.particleA.fixed).toBe(true);
+    expect(this.particleB.fixed).toBe(false);
   });
   it('should default [dimensions] to NDP.DIMENSIONS', function() {
+    expect(this.particleA.dimensions).toBe(NDP.DIMENSIONS);
   });
   it('should set [dimensions] to the fourth argument if passed', function() {
+    this.particleA = new NDP.Particle(1, 2, true, 3);
+    expect(this.particleA.dimensions).toBe(3);
   });
 
   describe('dimensions', function() {
