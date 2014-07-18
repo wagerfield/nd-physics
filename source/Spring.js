97c9fdfa4cf474371c912739ac3ec23c8bc330ea
Matthew Wagerfield <matthew@wagerfield.com>
2014-07-18 19:35:46 +0100

    :gb: Added constructor documentation

--- source/Spring.js
+++ source/Spring.js
@@ -1,4 +1,13 @@
-NDP.Spring = function() {
+/**
+ * Spring constructor.
+ * Creates a constraint between 2 particles.
+ * @constructor
+ * @param {Particle} p1 Particle to constrain to p2.
+ * @param {Particle} p2 Particle to constrain to p1.
+ * @param {Number} length Rest length of the spring.
+ * @param {Number} stiffness Stiffness of the spring where 0 is elastic and 1 is rigid.
+ */
+NDP.Spring = function(p1, p2, length, stiffness) {
 };
 
 NDP.Spring.prototype.update = function(delta, index) {
