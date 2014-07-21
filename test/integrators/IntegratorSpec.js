722356c9db876fe768dad538964e934c749ef498
Matthew Wagerfield <matthew@wagerfield.com>
2014-07-21 21:32:13 +0100

    :gb: Implemented dimensions property test suite

--- test/integrators/IntegratorSpec.js
+++ test/integrators/IntegratorSpec.js
@@ -2,9 +2,38 @@ describe('NDP.Integrator(opt_dimensions)', function() {
 
   beforeEach(function() {
     jasmine.addMatchers(CustomMatchers);
+    NDP.DIMENSIONS = 2;
+    this.integratorA = new NDP.Integrator();
+    this.integratorB = new NDP.Integrator(1);
   });
 
   it('should be a Function Object', function() {
     expect(NDP.Integrator).toEqual(jasmine.any(Function));
+  });
+  it('should default [dimensions] to NDP.DIMENSIONS', function() {
+    expect(this.integratorA.dimensions).toBe(NDP.DIMENSIONS);
+  });
+  it('should set [dimensions] to the first argument if passed', function() {
+    this.integratorA = new NDP.Integrator(1);
+    expect(this.integratorA.dimensions).toBe(1);
+
+    this.integratorB = new NDP.Integrator(2);
+    expect(this.integratorB.dimensions).toBe(2);
+  });
+  it('should only accept Numbers and set [dimensions] to an Integer', function() {
+    this.integratorA = new NDP.Integrator('1');
+    expect(this.integratorA.dimensions).toBe(NDP.DIMENSIONS);
+
+    this.integratorA = new NDP.Integrator('cats');
+    expect(this.integratorA.dimensions).toBe(NDP.DIMENSIONS);
+
+    this.integratorA = new NDP.Integrator(2.8);
+    expect(this.integratorA.dimensions).toBe(2);
+
+    this.integratorA = new NDP.Integrator(3.2);
+    expect(this.integratorA.dimensions).toBe(3);
+  });
+  it('should set [__dimensions] to [dimensions]', function() {
+    expect(this.integratorA.__dimensions).toBe(this.integratorA.dimensions);
   });
 });
