67dc7a3ced54797e2cf51e2f49320db4c1263988
Matthew Wagerfield <matthew@wagerfield.com>
2014-07-21 23:37:50 +0100

    :gb: Implemented EulerIntegrator integration function

--- source/integrators/EulerIntegrator.js
+++ source/integrators/EulerIntegrator.js
@@ -9,5 +9,28 @@
  */
 NDP.Integrator.create('EulerIntegrator',
   function(particle, delta, lubricity) {
+
+    // Calculate acceleration.
+    // acceleration *= inverseMass * delta
+    this.__vector.scale(particle.__acc, particle.__acc, particle.__inverseMass * delta);
+
+    // Add acceleration to velocity.
+    // velocity += acceleration
+    this.__vector.add(particle.__vel, particle.__vel, particle.__acc);
+
+    // Scale velocity by lubricity.
+    // velocity *= friction
+    this.__vector.scale(particle.__vel, particle.__vel, lubricity);
+
+    // Calculate velocity into slave to preserve momentum.
+    // velocity *= delta
+    this.__vector.scale(this.__vel, particle.__vel, delta);
+
+    // Add slave velocity to position.
+    // position += velocity
+    this.__vector.add(particle.__pos, particle.__pos, this.__vel);
+
+    // Reset acceleration.
+    this.__vector.identity(particle.__acc);
   }
 );
