3938d08c12140bfe5ebcea6bd26eefc534059b55
Matthew Wagerfield <matthew@wagerfield.com>
2014-07-21 21:14:24 +0100

    :gb: Added constructor and integrate documentation and removed id

--- source/integrators/Integrator.js
+++ source/integrators/Integrator.js
@@ -1,7 +1,17 @@
-NDP.Integrator = function() {
+/**
+ * Integrator constructor.
+ * Integrates particle motion.
+ * @constructor
+ * @param {Number} opt_dimensions Optional number of component dimension that the integrator should have. Defaults to NDP.DIMENSIONS.
+ */
+NDP.Integrator = function(opt_dimensions) {
 };
 
-NDP.Integrator.id = 'Integrator';
-
+/**
+ * Integrates motion for a collection of particles.
+ * @param {Array.<Particle>} particles Particle collection to integrate motion on.
+ * @param {Number} delta Time delta in milliseconds since last integration.
+ * @param {Number} lubricity Lubricity within the system.
+ */
 NDP.Integrator.prototype.integrate = function(particles, delta, lubricity) {
 };
