f75bdf5b2af84ad89c7593a5fcdea4fa0dc6a238
Matthew Wagerfield <matthew@wagerfield.com>
2014-07-21 23:43:10 +0100

    :gb: Implemented VerletIntegrator integration function

--- source/integrators/VerletIntegrator.js
+++ source/integrators/VerletIntegrator.js
@@ -9,5 +9,32 @@
  */
 NDP.Integrator.create('VerletIntegrator',
   function(particle, delta, lubricity) {
+
+    // Calculate velocity.
+    // velocity = position - oldPosition
+    this.__vector.subtract(particle.__vel, particle.__pos, particle.__old.pos);
+
+    // Scale velocity by lubricity.
+    // velocity *= friction
+    this.__vector.scale(particle.__vel, particle.__vel, lubricity);
+
+    // Calculate acceleration.
+    // acceleration *= delta * delta
+    this.__vector.scale(particle.__acc, particle.__acc, delta * delta);
+
+    // Add acceleration to velocity.
+    // velocity += acceleration
+    this.__vector.add(particle.__vel, particle.__vel, particle.__acc);
+
+    // Store old position.
+    // oldPosition = position
+    this.__vector.copy(particle.__old.pos, particle.__pos);
+
+    // Add velocity to position.
+    // position += velocity
+    this.__vector.add(particle.__pos, particle.__pos, particle.__vel);
+
+    // Reset acceleration.
+    this.__vector.identity(particle.__acc);
   }
 );
