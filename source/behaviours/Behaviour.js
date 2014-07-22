6507844dce4d0232cc491525275862046184fdae
Matthew Wagerfield <matthew@wagerfield.com>
2014-07-22 21:28:08 +0100

    :gb: Added constructor and method documentation comments

--- source/behaviours/Behaviour.js
+++ source/behaviours/Behaviour.js
@@ -1,8 +1,24 @@
-NDP.Behaviour = function() {
+/**
+ * Behaviour constructor.
+ * Applies forces to a particle.
+ * @constructor
+ * @param {Number} opt_dimensions Optional number of component dimension that the behaviour should have. Defaults to NDP.DIMENSIONS.
+ */
+NDP.Behaviour = function(opt_dimensions) {
   this.active = true;
 };
 
+/**
+ * Unique identifier.
+ * @type {String}
+ */
 NDP.Behaviour.id = 'Behaviour';
 
+/**
+ * Behaviour constructor.
+ * @param {Particle} particle Particle instance to apply the behaviour to.
+ * @param {Number} delta Time delta in milliseconds since last integration.
+ * @param {Number} index Index of the particle within the system.
+ */
 NDP.Behaviour.prototype.apply = function(particle, delta, index) {
 };
