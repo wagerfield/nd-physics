335943dee1a97e55361a12ac0a46432f240d7072
Matthew Wagerfield <matthew@wagerfield.com>
2014-07-22 21:48:03 +0100

    :gb: Added ConstantBehaviour constructor documentation comments

--- source/behaviours/ConstantBehaviour.js
+++ source/behaviours/ConstantBehaviour.js
@@ -1,1 +1,7 @@
+/**
+ * ConstantBehaviour constructor.
+ * Applies a constant force to the acceleration vector of a particle
+ * @constructor
+ * @param {String} namespace ConstantBehaviour namespace.
+ */
 var ConstantBehaviour = NDP.Behaviour.create('ConstantBehaviour');
