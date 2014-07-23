fda8efb37d1ca2b25de35bf9ec7826852eb42f2e
Matthew Wagerfield <matthew@wagerfield.com>
2014-07-23 21:41:44 +0100

    :gb: Started implementation of ConstantBehaviour

--- source/behaviours/ConstantBehaviour.js
+++ source/behaviours/ConstantBehaviour.js
@@ -1,9 +1,30 @@
 /**
  * ConstantBehaviour constructor.
- * Applies a constant force to the acceleration vector of a particle.
+ * Applies a constant force to the acceleration of a particle.
  * @constructor
  * @param {String} namespace ConstantBehaviour namespace.
  */
 (function(Behaviour) {
+
+  // this.__force = this.__vector.create();
+
+  // Object.defineProperty(Behaviour.prototype, 'force', {
+  //   set: function(value) {
+  //     if (NDP.isArray(value)) {
+  //       this.__force = value;
+  //     }
+  //   },
+  //   get: function() {
+  //     return this.__force;
+  //   }
+  // });
+
+  Behaviour.prototype.setForce = function(force) {
+    NDP.copyValuesToArray(force, this.__force);
+  };
+
+  Behaviour.prototype.apply = function(particle, delta, index) {
+    this.__vector.add(particle.__acc, particle.__acc, this.__force);
+  };
 
 })(NDP.Behaviour.create('ConstantBehaviour'));
