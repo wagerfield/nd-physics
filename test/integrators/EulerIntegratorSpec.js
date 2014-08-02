3fb2c7a617eda4482f94805f1036fe9253a5e21f
Matthew Wagerfield <matthew@wagerfield.com>
2014-08-02 23:13:37 +0100

    :gb: Added test for Integrator.__integrate(particle, delta, lubricity)

--- test/integrators/EulerIntegratorSpec.js
+++ test/integrators/EulerIntegratorSpec.js
@@ -2,9 +2,18 @@ describe('NDP.EulerIntegrator(opt_dimensions)', function() {
 
   beforeEach(function() {
     jasmine.addMatchers(CustomMatchers);
+    this.integrator = new NDP.EulerIntegrator();
+    this.engine = new NDP.Engine(this.integrator);
+    this.particle = new NDP.Particle(1);
   });
 
   it('should be a Function Object', function() {
     expect(NDP.EulerIntegrator).toEqual(jasmine.any(Function));
+  });
+
+  describe('__integrate(particle, delta, lubricity)', function() {
+    it('should integrate acceleration > velocity > position using Euler integration', function() {
+      this.integrator.__integrate(this.particle, 1, this.engine.lubricity);
+    });
   });
 });
--- test/integrators/VerletIntegratorSpec.js
+++ test/integrators/VerletIntegratorSpec.js
@@ -2,9 +2,18 @@ describe('NDP.VerletIntegrator(opt_dimensions)', function() {
 
   beforeEach(function() {
     jasmine.addMatchers(CustomMatchers);
+    this.integrator = new NDP.VerletIntegrator();
+    this.engine = new NDP.Engine(this.integrator);
+    this.particle = new NDP.Particle(1);
   });
 
   it('should be a Function Object', function() {
     expect(NDP.VerletIntegrator).toEqual(jasmine.any(Function));
+  });
+
+  describe('__integrate(particle, delta, lubricity)', function() {
+    it('should integrate acceleration > velocity > position using Euler integration', function() {
+      this.integrator.__integrate(this.particle, 1, this.engine.lubricity);
+    });
   });
 });
