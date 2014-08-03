bd9414d9bfa8666acf78a8174f4ccf6c06b1d8ce
Matthew Wagerfield <matthew@wagerfield.com>
2014-08-03 21:58:21 +0100

    :gb: Refactored acceleration calculation to use Particle force vector

--- source/integrators/VerletIntegrator.js
+++ source/integrators/VerletIntegrator.js
@@ -17,12 +17,9 @@ NDP.Integrator.create('VerletIntegrator',
   function(particle, delta, lubricity) {
 
     // Calculate acceleration.
-    // NOTE: Force is stored in the acceleration vector,
-    //       so needs to be converted to acceleration:
-    //       force = mass * acceleration
-    //       acceleration = force / mass || force * inverseMass
-    // acceleration *= inverseMass * delta * delta
-    this.__vector.scale(particle.__acc, particle.__acc, particle.__inverseMass * this.__deltaSquared);
+    // force = mass * acceleration
+    // acceleration = force / mass || force * inverseMass
+    this.__vector.scale(particle.__acc, particle.__force, particle.__inverseMass * this.__deltaSquared);
 
     // Calculate velocity.
     // velocity = position - oldPosition
@@ -44,7 +41,7 @@ NDP.Integrator.create('VerletIntegrator',
     // position += velocity
     this.__vector.add(particle.__pos, particle.__pos, particle.__vel);
 
-    // Reset acceleration.
-    this.__vector.identity(particle.__acc);
+    // Reset force.
+    this.__vector.identity(particle.__force);
   }
 );
